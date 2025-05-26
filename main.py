c9b6e5d4204abc543c06825f65560c9c82179a6b
juanpablow <juan.santana.macedo10@outlook.com>
2025-05-26 10:40:21 -0300

    feat: integrate `format_columns` with main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from email_utils import send_email_with_csv
-
-from scraper import scrap_zap_state
+from format_columns import format_columns
+from scraper import scrape_zap_state
 
 state = "sc"
 types = ["galpao-deposito-armazem"]
-exit_file = "imoveis_comerciais.csv"
+exit_file = "imoveis_comerciais.xlsx"
 
-scrap_zap_state(types, state, area_min=50, area_max=1000)
+scrape_zap_state(types, state, area_min=50, area_max=1000)
+
+format_columns(exit_file)
 
 send_email_with_csv(exit_file)
