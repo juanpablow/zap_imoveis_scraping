01f45a1e16298f5e687bdc121ac3a8dfa1bef878
juanpablow <juan.santana.macedo10@outlook.com>
2025-05-26 23:45:37 -0300

    feat: add multiple states to scrape

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+import pandas as pd
+
 from email_utils import send_email_with_csv
 from format_columns import format_columns
 from scraper import scrape_zap_state
@@ -6,9 +10,20 @@ states = ["sc", "rs"]
 types = ["galpao-deposito-armazem"]
 exit_file = "imoveis_comerciais.xlsx"
 
+now = datetime.now()
+date_filter = now.strftime("%d/%m/%Y")
+
+exit_file = f"imoveis_{date_filter.replace('/', '-')}.xlsx"
+
+results = []
 
 for state in states:
-    scrape_zap_state(types, state, area_min=50, area_max=1000)
+    df = scrape_zap_state(types, state, area_min=50, area_max=1000)
+
+    results.append(df)
+
+final_df = pd.concat(results, ignore_index=True)
+final_df.to_excel(exit_file, index=False)
 
 format_columns(exit_file)
 
